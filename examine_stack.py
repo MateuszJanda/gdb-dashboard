9f39b37eaabb0a9a95ebf0d57411a56988df4d78
Mateusz Janda <mateusz.janda@gmail.com>
2021-03-20 21:50:05 +0100

    Refactor

--- examine_stack.py
+++ examine_stack.py
@@ -3,8 +3,8 @@ import gdb
 # https://sourceware.org/gdb/onlinedocs/gdb/Commands-In-Python.html#Commands-In-Python
 
 
-ITEM_SIZE = 4  # for 32 bit architecture
-# ITEM_SIZE = 8  # for 64 bit architecture
+ADDR_SIZE = 4  # for 32 bit architecture
+# ADDR_SIZE = 8  # for 64 bit architecture
 
 
 class ExamineStack32(gdb.Command):
@@ -12,9 +12,9 @@ class ExamineStack32(gdb.Command):
         '''This registers our class as "examine_stack_32"'''
         super(ExamineStack32, self).__init__("examine_stack_32", gdb.COMMAND_DATA)
 
-    def invoke(self, num_of_items, from_tty):
-        '''When we call "examine_stack_32" from gdb, this method is call.'''
-        for i in range(0, int(num_of_items) * ITEM_SIZE, ITEM_SIZE):
+    def invoke(self, num_of_addrs, from_tty):
+        '''When we call "examine_stack_32" from gdb, this method is called.'''
+        for i in range(0, int(num_of_addrs) * ADDR_SIZE, ADDR_SIZE):
             gdb.execute('printf "Examine $esp+' + str(i) + ': 0x%0x\\n", $esp+' + str(i))
             try:
                 gdb.execute("x/x $esp+" + str(i))
